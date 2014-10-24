0c87c3a98c26725bfc55d8c445055eefb17cf4a2
I-NOZex <simplesmente_moi@hotmail.com>
2014-10-24 20:05:59 +0100

    Models Updated

--- FootballManager/Models/FMModel.Designer.cs
+++ FootballManager/Models/FMModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Jorge\Documents\github\Football-Manager\FootballManager\Models\FMModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\I-NOZex\documents\visual studio 2013\Projects\FootballManager\FootballManager\Models\FMModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
